8decd1b219516e0c62f2e3b8e472d8d36e9649d7
JoscaWij <Josca.W@gmx.net>
2020-08-18 15:21:35 +0200

    Add ListItem text and checkbox

---
+++ src/components/ListItemCheckbox.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ListItemCheckbox() {
+  return <input type="checkbox" />;
+}
+
+export default ListItemCheckbox;
---
+++ src/components/ListItemText.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ListItemText() {
+  return (
+    <div>
+      <div>Task title</div>
+      <span>Datum</span>
+    </div>
+  );
+}
+
+export default ListItemText;
