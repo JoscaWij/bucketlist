0a4f5aef2f7b39d0101c00b8dc21afdd9858dc96
JoscaWij <Josca.W@gmx.net>
2020-08-18 19:45:32 +0200

    Prepare back-btn in form

--- src/components/FormNewtodo.js
+++ src/components/FormNewtodo.js
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 
 function FormNewtodo({ onButtonClick }) {
   return (
-    <form>
-      <label>Title:</label>
-      <input placeholder="Title"></input>
-      <label>Date for task</label>
-      <input placeholder="Date in TT.MM.JJ"></input>
-      <button onClick={onButtonClick}>
-        <Link to="/todos">Create new task</Link>
-      </button>
-    </form>
+    <div>
+      <button>Back (ohne funct.)</button>
+      <form>
+        <label>Title:</label>
+        <input placeholder="Title"></input>
+        <label>Date for task</label>
+        <input placeholder="Date in TT.MM.JJ"></input>
+        <button onClick={onButtonClick}>
+          <Link to="/todos">Create new task</Link>
+        </button>
+      </form>
+    </div>
   );
 }
 
