9047c14d8e5a3bcdf28d66fbb9125901c241542d
Josca Wijtenburg <68224111+JoscaWij@users.noreply.github.com>
2020-08-18 15:35:45 +0200

    List (#3)
    * Add List-component
    
    * Add ListItem with img
    
    * Change port for api-fetch
    
    * Add ListItem text and checkbox
    
    * Stlye listItem and replace div with h3

---
+++ src/components/ListItemIcon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ImgSrc from "../assets/clipboard.svg";
+
+function ListItemIcon() {
+  return <img src={ImgSrc} alt="#" />;
+}
+
+export default ListItemIcon;
---
+++ src/components/ListItemText.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ListItemText() {
+  return (
+    <div>
+      <h3>Task title</h3>
+      <span>Datum</span>
+    </div>
+  );
+}
+
+export default ListItemText;
