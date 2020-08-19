6277cc904930176fc1ac7e9b8c3363140134aa06
JoscaWij <Josca.W@gmx.net>
2020-08-19 14:29:42 +0200

    Change title and delete comments in app.js

--- src/App.js
+++ src/App.js
@@ -10,23 +10,13 @@ import {
 } from "react-router-dom";
 import ToDos from "./pages/ToDos";
 import AddToDo from "./pages/AddToDo";
-import { createTodos } from "./api/todos";
 
 function App() {
-  /*   const handleData = (input) => {
-    const createToDo = async () => {
-      const newTodo = await createTodos(input);
-      console.log(newTodo);
-      setTodos(newTodo);
-    };
-    createToDo();
-  }; */
-
   return (
     <Router>
       <div className="app">
         <header className="app__header">
-          <h2>Title</h2>
+          <h2>To Do-Manager</h2>
         </header>
         <main className="app__main">
           <Switch>
