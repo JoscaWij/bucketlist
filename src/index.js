d6c582ad6a539076b6703dfdbaad45a2e0bd14cb
JoscaWij <Josca.W@gmx.net>
2020-08-20 15:32:05 +0200

    Refacture global style in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Global, css } from "@emotion/core";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        *,
+        *:after,
+        *:before {
+          box-sizing: border-box;
+        }
+
+        :root {
+          --highlightColor: #4462ff;
+        }
+
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+            "Helvetica Neue", sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+            monospace;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
