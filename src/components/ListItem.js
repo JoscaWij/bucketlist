06ff5b850e21213d82e9152f1717b979a78ad574
JoscaWij <Josca.W@gmx.net>
2020-08-20 13:58:53 +0200

    Refacture ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,29 @@
 import React from "react";
-import "./ListItem.css";
+import styled from "@emotion/styled";
+
+const Container = styled.a`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0;
+  border-bottom: 1px solid lightgrey;
+  text-decoration: none;
+
+  *:first-child,
+  *:last-child {
+    margin: 0 2em;
+  }
+
+  h3 {
+    margin: 0;
+  }
+`;
 
 function ListItem({ children, href }) {
   return (
-    <a className="listItem" href={href}>
+    <Container className="listItem" href={href}>
       {children}
-    </a>
+    </Container>
   );
 }
 
